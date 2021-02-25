ae1136399f1fc57c49c4fd04f6cc0f9483d45640
Alvaro Octal <alvaro.octal@gmail.com>
2021-02-25 18:00:05 +0100

    Minor changes

--- src/lib/tour-anchor.directive.ts
+++ src/lib/tour-anchor.directive.ts
@@ -1,15 +1,17 @@
-import { Directive, ElementRef, Host, HostBinding, Input } from '@angular/core';
-import type {OnDestroy, OnInit} from '@angular/core';
-import { PopperController, Placements, Triggers } from 'ngx-popper';
-import { TourAnchorDirective } from 'ngx-tour-core';
-import withinviewport from 'withinviewport';
+import { Directive, ElementRef, Host, HostBinding, Input } from "@angular/core";
+import type { OnDestroy, OnInit } from "@angular/core";
+import { PopperController, Placements, Triggers } from "ngx-popper";
+import { TourAnchorDirective } from "@ngx-tour/core";
+import withinviewport from "withinviewport";
 
-import { NgxpTourService } from './ngx-popper-tour.service';
-import { TourStepTemplateService } from './tour-step-template.service';
-import { INgxpStepOption as IStepOption } from './step-option.interface';
+import { NgxpTourService } from "./ngx-popper-tour.service";
+import { TourStepTemplateService } from "./tour-step-template.service";
+import { INgxpStepOption as IStepOption } from "./step-option.interface";
 
-@Directive({ selector: '[tourAnchor]'})
-export class TourAnchorNgxPopperPopoverDirective extends PopperController implements OnInit {
+@Directive({ selector: "[tourAnchor]" })
+export class TourAnchorNgxPopperPopoverDirective
+  extends PopperController
+  implements OnInit {
   // Overwrite parent ngOnInit to do nothing since the content property isn't set yet.
   ngOnInit() {}
 
@@ -20,20 +22,21 @@ export class TourAnchorNgxPopperPopoverDirective extends PopperController implem
 }
 
 @Directive({
-  selector: '[tourAnchor]',
+  selector: "[tourAnchor]",
 })
-export class TourAnchorNgxPopperDirective implements OnInit, OnDestroy, TourAnchorDirective {
+export class TourAnchorNgxPopperDirective
+  implements OnInit, OnDestroy, TourAnchorDirective {
   @Input() public tourAnchor: string;
 
-  @HostBinding('class.touranchor--is-active')
+  @HostBinding("class.touranchor--is-active")
   public isActive: boolean;
 
-  constructor(private tourService: NgxpTourService,
-              private tourStepTemplate: TourStepTemplateService,
-              private element: ElementRef,
-              @Host() private popoverDirective: TourAnchorNgxPopperPopoverDirective,
-  ) {
-  }
+  constructor(
+    private tourService: NgxpTourService,
+    private tourStepTemplate: TourStepTemplateService,
+    private element: ElementRef,
+    @Host() private popoverDirective: TourAnchorNgxPopperPopoverDirective
+  ) {}
 
   public ngOnInit(): void {
     this.tourService.register(this.tourAnchor, this);
@@ -46,30 +49,41 @@ export class TourAnchorNgxPopperDirective implements OnInit, OnDestroy, TourAnch
   public showTourStep(step: IStepOption): void {
     this.isActive = true;
     this.tourStepTemplate.templateComponent.step = step;
-    step.prevBtnTitle = step.prevBtnTitle || 'Prev';
-    step.nextBtnTitle = step.nextBtnTitle || 'Next';
-    step.endBtnTitle = step.endBtnTitle || 'End';
-    
+    step.prevBtnTitle = step.prevBtnTitle || "Prev";
+    step.nextBtnTitle = step.nextBtnTitle || "Next";
+    step.endBtnTitle = step.endBtnTitle || "End";
+
     this.popoverDirective.content = this.tourStepTemplate.template;
     this.popoverDirective.targetElement = this.element.nativeElement;
     this.popoverDirective.placement = step.placement || Placements.Auto;
     this.popoverDirective.showTrigger = Triggers.NONE;
 
-    if(step.popperSettings){
-      this.popoverDirective.boundariesElement = step.popperSettings.boundariesElement || undefined;
-      this.popoverDirective.closeOnClickOutside = step.popperSettings.closeOnClickOutside || false;
-      this.popoverDirective.disableAnimation = step.popperSettings.disableAnimation || false;
+    if (step.popperSettings) {
+      this.popoverDirective.boundariesElement =
+        step.popperSettings.boundariesElement || undefined;
+      this.popoverDirective.closeOnClickOutside =
+        step.popperSettings.closeOnClickOutside || false;
+      this.popoverDirective.disableAnimation =
+        step.popperSettings.disableAnimation || false;
       this.popoverDirective.disabled = step.popperSettings.disabled || false;
-      this.popoverDirective.disableStyle = step.popperSettings.disableStyle || false;
-      this.popoverDirective.forceDetection = step.popperSettings.forceDetection || false;
-      this.popoverDirective.hideOnClickOutside = step.popperSettings.hideOnClickOutside || false;
-      this.popoverDirective.hideOnScroll = step.popperSettings.hideOnScroll || false;
+      this.popoverDirective.disableStyle =
+        step.popperSettings.disableStyle || false;
+      this.popoverDirective["forceDetection"] =
+        step.popperSettings.forceDetection || false;
+      this.popoverDirective.hideOnClickOutside =
+        step.popperSettings.hideOnClickOutside || false;
+      this.popoverDirective.hideOnScroll =
+        step.popperSettings.hideOnScroll || false;
       this.popoverDirective.hideTimeout = step.popperSettings.hideTimeout || 0;
-      this.popoverDirective.positionFixed = step.popperSettings.positionFixed || false;
+      this.popoverDirective.positionFixed =
+        step.popperSettings.positionFixed || false;
       this.popoverDirective.showDelay = step.popperSettings.showDelay || 0;
-      this.popoverDirective.showOnStart = step.popperSettings.showOnStart || false;
-      this.popoverDirective.showTrigger = step.popperSettings.showTrigger || Triggers.NONE;
-      this.popoverDirective.timeoutAfterShow = step.popperSettings.timeoutAfterShow || 0;
+      this.popoverDirective.showOnStart =
+        step.popperSettings.showOnStart || false;
+      this.popoverDirective.showTrigger =
+        step.popperSettings.showTrigger || Triggers.NONE;
+      this.popoverDirective.timeoutAfterShow =
+        step.popperSettings.timeoutAfterShow || 0;
 
       //TODO: Can these even get passed in via json?
       // this.popoverDirective.popperModifiers = step.popperSettings.popperModifiers || undefined;
@@ -78,16 +92,21 @@ export class TourAnchorNgxPopperDirective implements OnInit, OnDestroy, TourAnch
     }
 
     this.popoverDirective.initialize();
-    if (step.hasOwnProperty('popperSettings') && step.popperSettings.hasOwnProperty('showDelay')) {
+    if (
+      step.hasOwnProperty("popperSettings") &&
+      step.popperSettings.hasOwnProperty("showDelay")
+    ) {
       this.popoverDirective.scheduledShow();
     } else {
       this.popoverDirective.show();
     }
 
     if (!step.preventScrolling) {
-      if (!withinviewport(this.element.nativeElement, { sides: 'bottom' })) {
+      if (!withinviewport(this.element.nativeElement, { sides: "bottom" })) {
         (<HTMLElement>this.element.nativeElement).scrollIntoView(false);
-      } else if (!withinviewport(this.element.nativeElement, { sides: 'left top right' })) {
+      } else if (
+        !withinviewport(this.element.nativeElement, { sides: "left top right" })
+      ) {
         (<HTMLElement>this.element.nativeElement).scrollIntoView(true);
       }
     }
