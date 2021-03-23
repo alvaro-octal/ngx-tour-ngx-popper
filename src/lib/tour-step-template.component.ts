88e9cf557cab0290446576298736a495df33207b
Kristof Marien <krmarien@gmail.com>
2021-03-23 20:46:36 +0100

    Update: improved template

--- src/lib/tour-step-template.component.ts
+++ src/lib/tour-step-template.component.ts
@@ -18,33 +18,21 @@ import { TourStepTemplateService } from './tour-step-template.service';
   selector: 'tour-step-template',
   template: `
     <popper-content class="popper-content">
-      <ng-container
-        *ngTemplateOutlet="
-          stepTemplate || stepTemplateContent || defaultTemplate;
-          context: { step: step }
-        "
-      ></ng-container>
+      <ng-container *ngTemplateOutlet="stepTemplate || stepTemplateContent || defaultTemplate;context: { step: step }">
+      </ng-container>
     </popper-content>
 
     <ng-template #defaultTemplate let-step="step">
       <p class="ngxp-title">{{ step?.title }}</p>
       <p class="ngxp-content">{{ step?.content }}</p>
       <div class="tour-step-navigation">
-        <button
-          [hidden]="!tourService.hasPrev(step)"
-          class="ngxp-btn btn-prev"
-          (click)="tourService.prev()"
-        >
-          « {{ step?.prevBtnTitle }}
-        </button>
-        <button
-          [hidden]="!tourService.hasNext(step)"
-          class="ngxp-btn btn-next"
-          (click)="tourService.next()"
-        >
-          {{ step?.nextBtnTitle }} »
-        </button>
-        <button class="ngxp-btn btn-end" (click)="tourService.end()">
+        <button [hidden]="!tourService.hasPrev(step)" class="btn btn-sm btn-primary" (click)="tourService.prev()">
+          {{ step?.prevBtnTitle }}
+        </button>&nbsp;
+        <button [hidden]="!tourService.hasNext(step)" class="btn btn-sm btn-primary" (click)="tourService.next()">
+          {{ step?.nextBtnTitle }}
+        </button>&nbsp;
+        <button class="btn btn-sm btn-primary" (click)="tourService.end()">
           {{ step?.endBtnTitle }}
         </button>
       </div>
